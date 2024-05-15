a23ce636c906e3b64d139086278ca2cf16d5b8ea
Lautaro Nievas <lautynievas09@gmail.com>
2024-05-15 15:22:43 -0300

    repo created

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const data = [
+  {
+    question:
+      "¿Qué método se utiliza para seleccionar un elemento del DOM por su ID?",
+    answers: [
+      "querySelector()",
+      "getElementByClass()",
+      "getElementById()",
+      "selectElementById()",
+    ],
+    correctAnswer: "getElementById()",
+  },
+  {
+    question: "¿Cuál es el operador de igualdad estricta en JavaScript?",
+    answers: ["===", "=", "==", "!=="],
+    correctAnswer: "===",
+  },
+  {
+    question:
+      "¿Qué método se utiliza para agregar un elemento al final de un array en JavaScript?",
+    answers: ["unshift()", "add()", "insert", "push()"],
+    correctAnswer: "push()",
+  },
+  {
+    question:
+      "¿Cuál es el método utilizado para obtener la longitud de un array en JavaScript?",
+    answers: ["size()", "count()", "sizeOf()", "length"],
+    correctAnswer: "length",
+  },
+  {
+    question: "¿Cómo se define una función anónima en JavaScript?",
+    answers: [
+      "function(){..}",
+      "anonymusFunction()",
+      "function myFunction(){..}",
+      "(function(){...})",
+    ],
+    correctAnswer: "function(){..}",
+  },
+];
+
+app.get("/quiz", async (req, res) => {
+  res.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
